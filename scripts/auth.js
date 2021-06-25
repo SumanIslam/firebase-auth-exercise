76610a2bf11a56135205000473197a24b520efce
SumanIslam <sumanislam@gmail.com>
2021-06-25 16:06:43 +0600

    get data from database

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,8 @@
+// get data
+db.collection('guides').get().then(snapshot => {
+  setupGuides(snapshot.docs);
+})
+
 // listen for auth status changes
 auth.onAuthStateChanged((user) => {
   if(user) {
@@ -7,7 +12,7 @@ auth.onAuthStateChanged((user) => {
   }
 })
 
-// sign up 
+// sign up a user
 const signUpForm = document.querySelector('#signup-form');
 
 signUpForm.addEventListener('submit', (e) => {
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,22 @@
+const guides = document.querySelector('.guides');
+
+// setup guides method
+const setupGuides = docs => {
+  let html = '';
+
+  docs.forEach(doc => {
+    const guide = doc.data();
+    const li = `
+      <li>
+        <div class="collapsible-header grey lighten-4">${guide.title}</div>
+        <div class="collapsible-body white">${guide.content}</div>
+      </li>
+    `;
+    html += li;
+  });
+  guides.innerHTML = html;
+}
+
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
 
